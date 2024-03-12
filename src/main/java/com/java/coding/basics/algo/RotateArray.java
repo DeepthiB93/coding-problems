5fb2c86710b56b7a33bacb12c2683724bb8102f1
Deepthi <deepthibhaskar16@gmail.com>
2024-03-11 23:21:49 -0400

    array probs

---
+++ src/main/java/com/java/coding/basics/algo/RotateArray.java
@@ -0,0 +1,31 @@
+package com.java.coding.basics.algo;
+
+public class RotateArray {
+	public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n; 
+        
+        reverse(nums, 0, n - 1);
+        
+        reverse(nums, 0, k - 1);
+        
+        reverse(nums, k, n - 1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+    public static void main(String[] args) {
+		int n[] = {1,2,3,4,5,6,7};
+		int k = 3;
+		RotateArray rt = new RotateArray();
+		rt.rotate(n, k);
+	}
+}
